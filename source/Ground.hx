3c5a15b2c0cc8b023e876a0cafc3f953896305cd
Bumgardner <sam125@live.missouristate.edu>
2019-04-03 18:14:38 -0500

    Added Ground objects.

---
+++ source/Ground.hx
@@ -0,0 +1,20 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+
+/**
+ * ...
+ * @author Sam
+ */
+class Ground extends FlxSprite 
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		makeGraphic(32, 32, FlxColor.GRAY);
+	}
+	
+}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,13 +1,23 @@
 package;
 
 import flixel.FlxState;
+import flixel.group.FlxGroup;
 import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
+	public var groundGrp:FlxGroup;
+	
 	override public function create():Void
 	{
 		super.create();
+		
+		groundGrp = new FlxGroup();
+		for (i in 0...10) {
+			groundGrp.add(new Ground(100 + 32 * i, 100));
+		}
+		add(groundGrp);
+		
 		var text = new FlxText(0, 0, 0, "hello world", 64);
 		text.screenCenter();
 		add(text);
