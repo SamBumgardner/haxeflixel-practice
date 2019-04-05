c0bcf7ca49d3a9a88def88b2bffff45f889cd833
Sam Bumgardner <sambumgardner1@gmail.com>
2019-04-04 19:39:15 -0500

    Set up environment to test Hero collisions in.
    The position of Ground objects were adjusted in PlayState to make them more
    comfortably positioned near the center of the screen.
    
    Collision was added to PlayState (and the Ground class was adjusted to
    be immovable) so we can see how a sprite can interact with solid ground.

--- source/Ground.hx
+++ source/Ground.hx
@@ -5,7 +5,10 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.util.FlxColor;
 
 /**
- * ...
+ * Simple class to represent immovable terrain (in this case, the ground).
+ * 
+ * Using FlxSprite like this isn't actually very performant, it's better to
+ *  use some sort of tilemap to represent static terrain.
  * @author Sam
  */
 class Ground extends FlxSprite 
@@ -13,8 +16,14 @@ class Ground extends FlxSprite
 
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
+		// Basic setup, should be familiar now.
 		super(X, Y, SimpleGraphic);
-		makeGraphic(32, 32, FlxColor.GRAY);
+		this.makeGraphic(32, 32, FlxColor.GRAY);
+		
+		// immovable is a property of FlxSprite used by Haxeflixel's built-in
+		//  collision-resolving system. Setting it to true means it can't be
+		//  pushed around by other objects.
+		this.set_immovable(true);
 	}
 	
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.group.FlxGroup;
@@ -7,6 +8,17 @@ import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
+	// I added some constant variables to the class definition.
+	// Rather than use random numbers in create(), we can reference
+	//  these variables that have some meaning attached to their names. 
+	private var TILE_SIZE(default, never):Int = 32;
+	
+	private var GROUND_POSITION_X(default, never):Int = 100;
+	private var GROUND_POSITION_Y(default, never):Int = 300;
+	
+	private var HERO_START_X(default, never):Int = 150;
+	private var HERO_START_Y(default, never):Int = 150;
+	
 	public var groundGrp:FlxGroup;
 	public var hero:Hero;
 	
@@ -16,17 +28,25 @@ class PlayState extends FlxState
 		
 		groundGrp = new FlxGroup();
 		for (i in 0...10) {
-			groundGrp.add(new Ground(100 + 32 * i, 100));
+			groundGrp.add(new Ground(GROUND_POSITION_X + TILE_SIZE * i, GROUND_POSITION_Y));
 		}
 		add(groundGrp);
 		
-		hero = new Hero(150, 50);
-		add(spr);
+		hero = new Hero(HERO_START_X, HERO_START_Y);
+		add(hero);
 		
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+		// Let everything update (which lets them move into each other)
 		super.update(elapsed);
+		
+		/////////////////////
+		// Collision logic //
+		/////////////////////
+		
+		// FlxG's collide checks for overlap, then separates overlapping objects.
+		FlxG.collide(hero, groundGrp);
 	}
 }
