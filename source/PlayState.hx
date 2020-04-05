659421fd5664f1d8aba043ad7f9b8abb6804ab16
SamBumgardner <sambumgardner1@gmail.com>
2020-04-05 17:55:26 -0500

    week-3-setup - reverted project back to defaults.
    Also includes some updates to make it in line with newer Haxe projects.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,22 +1,12 @@
 package;
 
-import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
-		var text = new FlxText(0, 0, 0, "hello world", 64);
-		text.screenCenter();
-		add(text);
-		
-		for (i in 0...15) {
-			var spr = new Hero(200, 200);
-			add(spr);
-		}
 	}
 
 	override public function update(elapsed:Float):Void
