2393d2780c02b9244d118a2c4f740d27dd7b010f
SamBumgardner <sambumgardner1@gmail.com>
2020-04-07 21:35:07 -0500

    collision-examples - platformer collision example.
    There's actually a fair amount of stuff going on in here. It's also
    nearly a game - all we need is a score counter and a system for spawning
    more fireballs and we're set.
    
    Some lines need to be un-commented to unlock the full functionality of
    the example. During class we'll work through it one step at a time.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,25 @@
 package;
 
+import entities.projectiles.Fireball;
+import flixel.FlxObject;
+import flixel.FlxG;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import entities.player.Hero;
+import entities.terrain.Wall;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-	static private var hero:Hero;
+	private static var WALL_COUNT(default, never) = 10;
+	private static var WALL_START_X(default, never) = 150;
+	private static var WALL_START_Y(default, never) = 200;
+
+	private static var FIREBALL_COUNT(default, never) = 5;
+	private static var FIREBALL_SPAWN_BORDER(default, never) = 50;
+
+	private var hero:Hero;
+	private var walls:FlxTypedGroup<Wall>;
+	private var fireballs:FlxTypedGroup<Fireball>;
 
 	override public function create():Void
 	{
@@ -13,10 +27,74 @@ class PlayState extends FlxState
 
 		hero = new Hero();
 		add(hero);
+
+		initializeWalls();
+		initializeFireballs();
+	}
+
+	private function initializeWalls() {
+		walls = new FlxTypedGroup<Wall>();
+
+		for (i in 0...WALL_COUNT) {
+			var x:Float = WALL_START_X + (i * Wall.WIDTH);
+			var y:Float = WALL_START_Y;
+			var wall:Wall = new Wall(x, y);
+			walls.add(wall);
+		}
+		add(walls);
+	}
+
+	private function initializeFireballs() {
+		fireballs = new FlxTypedGroup<Fireball>();
+
+		for (i in 0...FIREBALL_COUNT) {
+			var x:Float = FlxG.random.int(FIREBALL_SPAWN_BORDER, 
+				FlxG.width - FIREBALL_SPAWN_BORDER);
+			var y:Float = FlxG.random.int(FIREBALL_SPAWN_BORDER, 
+				FlxG.height - FIREBALL_SPAWN_BORDER);
+			var fireball = new Fireball(x, y);
+			fireballs.add(fireball);
+		}
+		add(fireballs);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
+		// Uncomment to collide hero against all wall objects.
+		//FlxG.collide(hero, walls);
+
+		// Uncomment to trigger custom logic when a player overlaps with a fireball.
+		//FlxG.overlap(hero, fireballs, resolveHeroFireballOverlap);
+
+		// Wrap various objects if gone offscreen.
+		screenWrapObject(hero);
+		for (fireball in fireballs) {
+			screenWrapObject(fireball);
+		}
+	}
+
+	private function screenWrapObject(wrappingObject:FlxObject) {
+		if (wrappingObject.x > FlxG.width) {
+			wrappingObject.x = 0 - wrappingObject.width;
+		} else if (wrappingObject.x + wrappingObject.width < 0) {
+			wrappingObject.x = FlxG.width;
+		}
+
+		if (wrappingObject.y > FlxG.height) {
+			wrappingObject.y = 0 - wrappingObject.height;
+		} else if (wrappingObject.y + wrappingObject.height < 0) {
+			wrappingObject.y = FlxG.height;
+		}
+	}
+
+	/**
+		Function called when an overlap between hero and fireball is detected.
+	**/
+	private function resolveHeroFireballOverlap(hero:Hero, fireball:Fireball) {
+		trace("Hero and Fireball collided!");
+		// fireball.kill();
+		// hero.kill();
 	}
 }
